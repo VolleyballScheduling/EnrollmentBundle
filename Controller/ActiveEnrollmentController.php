67109e0133739f7513944e468240abac0b707a07
david durost <david.durost@gmail.com>
2014-12-28 13:23:35 -0500

    active enrollment enhancements Signed-off-by:david durost <david.durost@gmail.com>

--- Controller/ActiveEnrollmentController.php
+++ Controller/ActiveEnrollmentController.php
@@ -1,6 +1,43 @@
 <?php
 namespace Volleyball\Bundle\EnrollmentBundle\Controller;
 
+use \Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use \Symfony\Component\HttpFoundation\Request;
+
 class ActiveEnrollmentController extends \Volleyball\Bundle\UtilityBundle\Controller\UtilityController
 {
+    public function organizationAction(Request $request)
+    {
+        return array();
+    }
+    
+    public function councilAction(Request $request)
+    {
+        return array();
+    }
+    
+    public function regionAction(Request $request)
+    {
+        return array();
+    }
+    
+    public function passelAction(Request $request)
+    {
+        return array();
+    }
+    
+    public function facilityAction(Request $request)
+    {
+        return array();
+    }
+    
+    public function seasonAction(Request $request)
+    {
+        return array();
+    }
+    
+    public function weekAction(Request $request)
+    {
+        return array();
+    }
 }
